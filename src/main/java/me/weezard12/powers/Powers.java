58297dee315d0704eaf3ee216943dfa91c21b792
weezard12 <weezard1234@gmail.com>
2026-02-06 22:40:25 +0200

    init commit

---
+++ src/main/java/me/weezard12/powers/Powers.java
@@ -0,0 +1,17 @@
+package me.weezard12.powers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Powers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
